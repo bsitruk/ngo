81f2124672765ff445ff3c2e796226914c21279b
jonathan <jonathan.attali23@gmail.com>
2018-06-20 15:52:18 +0300

    card

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import Project from './Project/Project.js'
 import './App.css';
 
 class App extends Component {
@@ -13,6 +14,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Project src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqhB6oels3yCLW6CIHxj2iY8lLrXA5ReMbWO10uc3mM23XVByB" title="ABUGOSH SOCCER GAME" goal="change the world" 
+        activity="football game in tel aviv" budget="200$"/>
+
       </div>
     );
   }
