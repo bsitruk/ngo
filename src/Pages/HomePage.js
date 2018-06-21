099cb89644122b1c70f2ecd379deb1700ec0eec1
jonathan <jonathan.attali23@gmail.com>
2018-06-21 12:45:49 +0300

    some CSS updates

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "./HomePage.css";
-import Mybutton from "../Component/button" ;
+import Mybutton from "../Component/button";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 class HomePage extends Component {
     constructor(props) {
@@ -10,21 +10,21 @@ class HomePage extends Component {
     render() {
         return (
             <div>
-                 <div class="Mainpage">
-            <h1 class="Heading">WHERE THERE IS UNITY THERE IS ALWAYS VICTORY
-            <br/><Mybutton classes="btn btn-primary btn-lg"></Mybutton></h1>
-            </div>
+                <div class="Mainpage">
+                    <h1 class="Heading">WHERE THERE IS UNITY THERE IS ALWAYS VICTORY
+            <br /><Mybutton classes=" communityBtn btn btn-primary btn-lg">Join our Community of NGO'S</Mybutton></h1>
+                </div>
                 <div class="container">
                     <div class="row text-center quotebox">
                         <div class=" quote col-md-6">
 
                             <p class="subText">
-                            Our mission is to connect National Government Organization's through the means of shared sporting events. Each
- NGO has the opportunity to choose between joining an existing event and creating a new event. Through our platform, relationships and friendships will be formed in the most unlikely places. 
-                                <br /> Join us today! Create Unity - Change the World! :)"<br /><br/>
-                                
+                                Our mission is to connect National Government Organization's through the means of shared sporting events. Each
+     NGO has the opportunity to choose between joining an existing event and creating a new event. Through our platform, relationships and friendships will be formed in the most unlikely places.
+                                <br /> Join us today! Create Unity - Change the World! :)"<br /><br />
+
                                 <form action="/events">
-                                <button type="link" class="btn btn-primary btn-lg">Join an Event</button>
+                                    <button type="link" class="btn btn-primary btn-lg">Join an Event</button>
                                 </form>
                             </p>
                         </div>
@@ -37,18 +37,18 @@ class HomePage extends Component {
 
                     <div class="row quote">
                         <div class="col-md-6 secondImage">
-                        <div class="picture">
+                            <div class="picture">
                                 <img class="football" src="http://wp.patheos.com.s3.amazonaws.com/blogs/paperbacktheology/files/2013/06/playing-baseball.jpg" />
                             </div>
-                            </div>
+                        </div>
 
                         <div class="col-md-6">
                             <p class="rightbox">
-                            “We
- become not a melting pot but a beautiful mosaic. Different people, different beliefs, different yearnings, different hopes, different dreams.”
- <br/>
- <br/>                          <form action="/new-event">
-                                <button  class="btn btn-primary btn-lg">Create an Event</button>
+                                “We
+     become not a melting pot but a beautiful mosaic. Different people, different beliefs, different yearnings, different hopes, different dreams.”
+ <br />
+                                <br />                          <form action="/new-event">
+                                    <button class="btn btn-primary btn-lg">Create an Event</button>
                                 </form>
                             </p>
                         </div>
